505806906c32aaa603735e8dc2ae0c3ba73d0e0e
GoracioNewport <bdfyljkub@gmail.com>
2022-07-24 23:40:06 +0600

    Export Fix

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ export const API_URL = 'https://82.200.48.68:8000/';
 
 const https = require('https')
 
-const agent = new https.Agent({
+export const agent = new https.Agent({
   rejectUnauthorized: false,
   requestCert: false,
   agent: false,
