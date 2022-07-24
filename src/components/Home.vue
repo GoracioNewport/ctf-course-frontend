05f275c5e0b97ab7e23c53a27c1d21879a3d8d0f
GoracioNewport <bdfyljkub@gmail.com>
2022-07-25 02:34:47 +0600

    Home page styling

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,4 +1,19 @@
 <template lang="pug">
-.wrapper.d-flex.justify-content-center.my-md-5
-  h1.display-2 <b>G</b>oracio's <b>C</b>apture <b>T</b>he <b>F</b>lag course 
+.wrapper.d-flex.flex-column.my-md-5.text-center
+  h1.display-2.mt-3 <b>G</b>oracio's <b>C</b>apture <b>T</b>he <b>F</b>lag course 
+  .text-block.d-flex.flex-column.text-start.mx-2.my-5.mx-md-auto
+    span Добро пожаловать!
+    span Это главная страница.
+    span.mt-5 <b>Материалы</b> - тут ты найдешь полезные заметки, которые будут полезны для решения задач.
+    span <b>Таски</b> - разбиты на разделы. Один раздел в день - не больше! 
+    span.mb-5 <b>Таблица лидеров</b> - самым лучшим игрокам гарантирую приз :)
+    span
+    span Чего же ты ждешь, авторизуйся и вперед!
 </template>
+
+<style scoped>
+span {
+  font-family: 'Courier New', Courier, monospace;
+  font-size: 24px
+}
+</style>
