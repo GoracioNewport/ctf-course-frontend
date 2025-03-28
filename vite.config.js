b53050f2e4b04553b5a1f8d053047407b604c3e6
Ivan Dolgikh <bdfyljkub@gmail.com>
2025-03-29 00:56:27 +0300

    feat: Docker

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,17 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    port: 3000,
+    host: true,
+    proxy: {
+      '/api': {
+        target: 'http://backend:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        secure: false
+      }
+    }
+  }
 })
